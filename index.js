2d43ba033fb672f2cb31ca21b486c290b282866e
Gordon Cheng <trgordonb@gmail.com>
2022-04-12 00:29:42 +0800

    use env variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT
 const tableRouter = require("./routes/table");
 app.use(express.json());
 app.use(
